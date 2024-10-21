1e8de02d9da73cd6688ff64f796a28ba7f87b4a0
lvdhamija <lvdhamija@gmail.com>
2024-10-21 10:37:28 +0530

    add new file booleandatatype

---
+++ datatype/ByteDatatype.java
@@ -0,0 +1,22 @@
+class ByteDatatype
+{
+    public static void main (String[] args)
+    {
+        byte b = 127;
+        int c = 128;
+        double d = 10.5;
+        boolean bo = true;
+        int x = 2147483647;
+        System.out.println("Byte  variable : "+b);
+        System.out.println("Int variable : "+c);
+        System.out.println("double  variable : "+d);
+        System.out.println("Byte  variable : "+bo);
+        System.out.println("Byte  variable : "+x);
+            System.out.println("Hello World");
+
+        System.out.println("Byte  variable : " + b
+        + " Integer variable : "+c+" double variable : "+ d);
+
+
+    }
+}
