410e8fa0d44958b4905845cd6327588a1e05280d
lvdhamija <lvdhamija@gmail.com>
2024-10-22 11:28:33 +0530

    add new class

---
+++ datatype/DefaultPrimitiveTest.java
@@ -0,0 +1,30 @@
+class DefaultPrimitiveTest
+{
+    public static void main (String[] args)
+    {
+        byte b;// this is declaration :
+        b = 9;// this is initialization :
+        short s;// this is declaration :
+        s = 10;// this is initialization :
+        int i;// this is declaration :
+        i= 11;// this is initialization :
+        long l;//this is declaration :
+        l=12;// this is initialization :
+        float f;//this is declaration :
+        f=10.9f;// this is initialization :
+        double d;//this is declaration :
+        d=11.9;// this is initialization :
+        boolean bo;//this is declaration :
+        bo = false;// this is initialization :
+        char ch;//this is declaration :
+        ch= 11;// this is initialization :
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(bo);
+        System.out.println(ch);
+    }
+}
